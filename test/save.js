a6799f52b379c74790a09b0cd89deaa0b6a67474
isaacs <i@izs.me>
2012-08-14 17:33:56 -0700

    test

---
+++ test/save.js
@@ -0,0 +1,48 @@
+var test = require('tap').test
+var npmconf = require('../npmconf.js')
+var common = require('./00-setup.js')
+var fs = require('fs')
+var ini = require('ini')
+var expect =
+  [ 'globalconfig = /Users/isaacs/dev/js/npmconf/test/fixtures/globalconfig',
+    'email = i@izs.me',
+    'env-thing = asdf',
+    'init.author.name = Isaac Z. Schlueter',
+    'init.author.email = i@izs.me',
+    'init.author.url = http://blog.izs.me/',
+    'proprietary-attribs = false',
+    'npm:publishtest = true',
+    '_npmjs.org:couch = https://admin:password@localhost:5984/registry',
+    '_auth = dXNlcm5hbWU6cGFzc3dvcmQ=',
+    'npm-www:nocache = 1',
+    'sign-git-tag = false',
+    'message = v%s',
+    'strict-ssl = false',
+    '',
+    '[_token]',
+    'AuthSession = yabba-dabba-doodle',
+    'version = 1',
+    'expires = 1345001053415',
+    'path = /',
+    'httponly = true',
+    '' ].join('\n')
+
+test('saving configs', function (t) {
+  npmconf.load(function (er, conf) {
+    if (er)
+      throw er
+    conf.set('sign-git-tag', false, 'user')
+    conf.del('nodedir')
+    conf.del('tmp')
+    var foundConf = ini.stringify(conf.sources.user.data)
+    t.equal(foundConf, expect)
+    conf.save('user', function (er) {
+      if (er)
+        throw er
+      var uc = fs.readFileSync(conf.get('userconfig'), 'utf8')
+      t.equal(uc, expect)
+      t.end()
+    })
+  })
+})
+
