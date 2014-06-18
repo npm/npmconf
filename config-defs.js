4dbf452dc1539b0b2f01375bd1687ab06534dd32
Forrest L Norvell <forrest@npmjs.com>
2014-06-18 15:46:41 -0700

    add new parameter for scope

--- config-defs.js
+++ config-defs.js
@@ -190,6 +190,7 @@ Object.defineProperty(exports, "defaults", {get: function () {
     , "save-exact" : false
     , "save-optional" : false
     , "save-prefix": "^"
+    , scope : ""
     , searchopts: ""
     , searchexclude: null
     , searchsort: "name"
@@ -294,6 +295,7 @@ exports.types =
   , "save-exact" : Boolean
   , "save-optional" : Boolean
   , "save-prefix": String
+  , scope : String
   , searchopts : String
   , searchexclude: [null, String]
   , searchsort: [ "name", "-name"
