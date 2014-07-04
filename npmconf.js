npm/npmconf

commit e5dcca48a191658538108f48a8dd5f9aaa8f52a0

Simply _env stuff, add test for same

npmconf.js
@@ -352,20 +352,16 @@ Conf.prototype.addEnv = function (env) {
   env = env || process.env
   var conf = {}
   Object.keys(env)
-    .filter(function (k) { return k.match(/^npm_config__?/i) })
+    .filter(function (k) { return k.match(/^npm_config_/i) })
     .forEach(function (k) {
       if (!env[k])
         return
 
       // leave first char untouched, even if
       // it is a "_" - convert all other to "-"
-      var t = k.replace(/^npm_config_/i, '')
-          .toLowerCase()
-      var s = t.substr(1)
-          .replace(/_/g, '-')
-      var p = t.substr(0, 1)
-
-      conf[p + s] = env[k]
+      var p = k.replace(/^npm_config_/, '')
+               .replace(/(?!^)_/g, '-')
+      conf[p] = env[k]
     })
   return CC.prototype.addEnv.call(this, '', conf, 'env')
 }

test/00-setup.js
@@ -14,6 +14,19 @@ Object.keys(process.env).forEach(function (k) {
 process.env.npm_config_userconfig = exports.userconfig
 process.env.npm_config_other_env_thing = 1000
 process.env.random_env_var = 'asdf'
+process.env.npm_config__underbar_env_thing = 'underful'
+
+exports.envData = {
+  userconfig: exports.userconfig,
+  '_underbar-env-thing': 'underful',
+  'other-env-thing': '1000'
+}
+exports.envDataFix = {
+  userconfig: exports.userconfig,
+  '_underbar-env-thing': 'underful',
+  'other-env-thing': 1000,
+}
+
 
 if (module === require.main) {
   // set the globalconfig in the userconfig

test/basic.js
@@ -31,8 +31,8 @@ var ucData =
        path: '/',
        httponly: true } }
 
-var envData = { userconfig: common.userconfig, 'other-env-thing': '1000' }
-var envDataFix = { userconfig: common.userconfig, 'other-env-thing': 1000 }
+var envData = common.envData
+var envDataFix = common.envDataFix
 
 var gcData = { 'package-config:foo': 'boo' }
 

test/builtin.js
@@ -29,8 +29,8 @@ var ucData =
        path: '/',
        httponly: true } }
 
-var envData = { userconfig: common.userconfig, 'other-env-thing': '1000' }
-var envDataFix = { userconfig: common.userconfig, 'other-env-thing': 1000 }
+var envData = common.envData
+var envDataFix = common.envDataFix
 
 var gcData = { 'package-config:foo': 'boo' }
 

test/project.js
@@ -33,8 +33,8 @@ var ucData =
        path: '/',
        httponly: true } }
 
-var envData = { userconfig: common.userconfig, 'other-env-thing': '1000' }
-var envDataFix = { userconfig: common.userconfig, 'other-env-thing': 1000 }
+var envData = common.envData
+var envDataFix = common.envDataFix
 
 var gcData = { 'package-config:foo': 'boo' }
 

commit 016b2988e52f1dc1f95a19f124ac76c714228b78

reinstate support for upper-case npm_config_* environment variables
This fixes a regression introduced in e5dcca4.

npmconf.js
@@ -338,7 +338,8 @@ Conf.prototype.addEnv = function (env) {
 
       // leave first char untouched, even if
       // it is a "_" - convert all other to "-"
-      var p = k.replace(/^npm_config_/, '')
+      var p = k.toLowerCase()
+               .replace(/^npm_config_/, '')
                .replace(/(?!^)_/g, '-')
       conf[p] = env[k]
     })

test/00-setup.js
@@ -15,15 +15,18 @@ process.env.npm_config_userconfig = exports.userconfig
 process.env.npm_config_other_env_thing = 1000
 process.env.random_env_var = 'asdf'
 process.env.npm_config__underbar_env_thing = 'underful'
+process.env.NPM_CONFIG_UPPERCASE_ENV_THING = 42
 
 exports.envData = {
   userconfig: exports.userconfig,
   '_underbar-env-thing': 'underful',
+  'uppercase-env-thing': '42',
   'other-env-thing': '1000'
 }
 exports.envDataFix = {
   userconfig: exports.userconfig,
   '_underbar-env-thing': 'underful',
+  'uppercase-env-thing': 42,
   'other-env-thing': 1000,
 }
 